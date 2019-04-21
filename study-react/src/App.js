6999ac797a4905d2058bab95d7bd889984ececc9
shiyou00 <8533334@qq.com>
2019-04-21 16:54:01 +0800

    add case

--- study-react/src/App.js
+++ study-react/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import JsxCom from './JsxCom';
+import ListOfTenThings from './Repeat';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <JsxCom />
+        <ListOfTenThings />
       </div>
     );
   }
