a9f4dc8c8a9228458a9955f14d7128370badfe46
shiyou <8533334@qq.com>
2019-09-27 10:43:35 +0800

    sku

---
+++ react-todoList/src/pages/hook/Hook.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+export function Hook() {
+  const [num, setNum] = useState(0);
+
+  useEventListener('resize', function (e) {
+    console.log(e);
+  });
+
+  return (
+    <div>
+      <p>You clicked {num} times</p>
+      <button onClick={()=>{ setNum(num + 1) }}>
+        Click me
+      </button>
+    </div>
+  );
+}
+
+// Hook
+function useEventListener(eventName, handler, element = window){
+  // Create a ref that stores handler
+  const savedHandler = useRef();
+
+  // Update ref.current value if handler changes.
+  // This allows our effect below to always get latest handler ...
+  // ... without us needing to pass it in effect deps array ...
+  // ... and potentially cause effect to re-run every render.
+  useEffect(() => {
+    savedHandler.current = handler;
+  }, [handler]);
+
+  useEffect(
+    () => {
+      // Make sure element supports addEventListener
+      // On
+      const isSupported = element && element.addEventListener;
+      if (!isSupported) return;
+
+      // Create event listener that calls handler function stored in ref
+      const eventListener = event => savedHandler.current(event);
+
+      // Add event listener
+      element.addEventListener(eventName, eventListener);
+
+      // Remove event listener on cleanup
+      return () => {
+        element.removeEventListener(eventName, eventListener);
+      };
+    },
+    [eventName, element] // Re-run if eventName or element changes
+  );
+}
--- react-todoList/src/pages/todo/todo.js
+++ react-todoList/src/pages/todo/todo.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './todo.css';
 import { TodoInput } from "../../components/header/todoInput";
 import { TodoWrap } from "../../components/body/todoWrap";
+import { Hook } from "../hook/Hook";
+import { Sku, WordAdder } from "../sku/Sku";
 
 class Todo extends Component {
 
@@ -10,6 +12,9 @@ class Todo extends Component {
       <div className="app">
         <TodoInput />
         <TodoWrap />
+        <Hook />
+        <Sku />
+        <WordAdder />
       </div>
     );
   }
